01e1ddcaee977f741e71846cf9b4155d92179ec6
ViAR-Black <favoritesing2@gmail.com>
2025-10-08 19:39:07 +0300

    Db connect (#2)
    * Change_db
    
    * Made close\open func
    
    * add_requrements.txt

--- app/core/db.py
+++ app/core/db.py
@@ -1,8 +1,30 @@
-dictionary = {}
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+from psycopg import AsyncConnection
+from psycopg_pool import AsyncConnectionPool
 
-# class ObsConnection:
-#     def __init__():
-#         pass
 
-#     async def get_connect():
-#         pass
+class DatabaseConnection:    
+    def __init__(self, settings):
+        self.pool = AsyncConnectionPool(
+            conninfo=settings.database.db_uri,
+            min_size=settings.database.min_pool_size,
+            max_size=settings.database.max_pool_size,
+        )
+    
+    @asynccontextmanager
+    async def session(self) -> AsyncIterator[AsyncConnection]:
+        async with self.pool.connection() as conn:
+            yield conn
+    
+    @asynccontextmanager
+    async def transaction(self) -> AsyncIterator[AsyncConnection]:
+        async with self.pool.connection() as conn:
+            async with conn.transaction():
+                yield conn
+
+    async def open(self):
+        await self.pool.open()
+
+    async def close(self):
+        await self.pool.close()
